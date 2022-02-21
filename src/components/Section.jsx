0ec321542989eaf3b8e7a79ea53a49261263b971
rasangalk <rasangalakshith38@gmail.com>
2022-02-21 13:31:01 +0530

    everything completed without redux

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,21 +1,26 @@
 import React from "react";
 import styled from "styled-components";
+import Fade from "react-reveal/Fade";
 
 function Section(props) {
 	return (
 		<Wrap bgImg={props.backgroundImg}>
-			<ItemText>
-				<h1>{props.title}</h1>
-				<p>{props.description}</p>
-			</ItemText>
+			<Fade bottom>
+				<ItemText>
+					<h1>{props.title}</h1>
+					<p>{props.description}</p>
+				</ItemText>
+			</Fade>
 
 			<Buttons>
-				<ButtonGroup>
-					<LeftButton>{props.leftBtnText}</LeftButton>
-					{props.rightBtnText && (
-						<RightButton>{props.rightBtnText}</RightButton>
-					)}
-				</ButtonGroup>
+				<Fade bottom>
+					<ButtonGroup>
+						<LeftButton>{props.leftBtnText}</LeftButton>
+						{props.rightBtnText && (
+							<RightButton>{props.rightBtnText}</RightButton>
+						)}
+					</ButtonGroup>
+				</Fade>
 
 				<DownArrow src='/images/down-arrow.svg' />
 			</Buttons>
@@ -26,6 +31,7 @@ function Section(props) {
 export default Section;
 
 const Wrap = styled.div`
+	z-index: 10;
 	width: 100vw;
 	height: 100vh;
 	background-image: url("/images/model-3.jpg");
