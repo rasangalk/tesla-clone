30d1499b8997c2591d015e0464e78ac919b0f850
rasangalk <rasangalakshith38@gmail.com>
2022-02-24 14:20:32 +0530

    project completed
    added redux slice to store menu items in the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,28 +2,25 @@ import React, { useState } from "react";
 import styled from "styled-components";
 import MenuIcon from "@mui/icons-material/Menu";
 import CloseIcon from "@mui/icons-material/Close";
+import { selectCars } from "../features/car/carSlice";
+import { useSelector } from "react-redux";
 
 function Header() {
 	const [burgerStatus, setBurgerStatus] = useState(false);
+	const cars = useSelector(selectCars);
 
 	return (
 		<Container>
 			<a>
-				<img src='/images/logo.svg' alt='logo' />{" "}
+				<img src='/images/logo.svg' alt='logo' />
 			</a>
 
 			<Menu>
-				<a href='#'>Model S</a>
-
-				<a href='#'>Model 3</a>
-
-				<a href='#'>Model X</a>
-
-				<a href='#'>Model Y</a>
-
-				<a href='#'>Solar Roof</a>
-
-				<a href='#'>Solar Panels</a>
+				{cars.map((car, index) => (
+					<a key={index} href='#'>
+						{car}
+					</a>
+				))}
 			</Menu>
 
 			<RightMenu>
